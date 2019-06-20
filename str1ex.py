4befc7da2bbbda70c8e7920538206d40dcb4c4d8
John Vincent Reyes <jvincentreyes@gmail.com>
2019-06-20 14:03:12 -0700

    str 1 exercise

---
+++ str1ex.py
@@ -0,0 +1,11 @@
+name = "John Reyes"
+name2 = "Elise Giongco"
+name3 = "Aiden Reyes"
+
+print("{:>30}".format(name))
+print("{:>30}".format(name2))
+print("{:>30}".format(name3))
+
+name4 = input("Enter name 4: ")
+
+print("{:>30}".format(name4))
